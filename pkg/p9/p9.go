9925ea37d5a727f5772c1ad9d8056e4249df9347
Fravic <fravicf@gmail.com>
2023-12-08 23:02:52 -0800

    autoformat

--- pkg/p9/p9.go
+++ pkg/p9/p9.go
@@ -6,31 +6,31 @@ import (
 )
 
 func ReadInput() []string {
-  content, _ := os.ReadFile("input/p9.txt")
-  lines := strings.Split(string(content), "\n")
-  return lines
+	content, _ := os.ReadFile("input/p9.txt")
+	lines := strings.Split(string(content), "\n")
+	return lines
 }
 
 func DiffSequence(seq []int) *[]int {
-  var diffSlice []int
-  for i := 1; i < len(seq); i++ {
-    diffSlice = append(diffSlice, seq[i] - seq[i - 1])
-  }
-  return &diffSlice
+	var diffSlice []int
+	for i := 1; i < len(seq); i++ {
+		diffSlice = append(diffSlice, seq[i]-seq[i-1])
+	}
+	return &diffSlice
 }
 
 func HasNonZero(seq []int) bool {
-  for _, num := range seq {
-    if num != 0 {
-      return true;
-    }
-  }
-  return false
+	for _, num := range seq {
+		if num != 0 {
+			return true
+		}
+	}
+	return false
 }
 
 func Reverse[T any](s []T) {
-  last := len(s) - 1
-  for i := 0; i < len(s)/2; i++ {
-    s[i], s[last-i] = s[last-i], s[i]
-  }
+	last := len(s) - 1
+	for i := 0; i < len(s)/2; i++ {
+		s[i], s[last-i] = s[last-i], s[i]
+	}
 }
--- pkg/p9/p9_2/p9_2.go
+++ pkg/p9/p9_2/p9_2.go
@@ -8,31 +8,30 @@ import (
 )
 
 func Run() {
-  lines := p9.ReadInput()
-  sum := 0
-  for _, line := range lines {
-    numbers := strings.Split(line, " ")
-    var intSlice []int
-    for _, strNum := range numbers {
-      num, _ := strconv.Atoi(strNum)
-      intSlice = append(intSlice, num)
-    }
-    sum += calculateNextInSequence(intSlice)
-  }
-  fmt.Println("Result: ", sum)
+	lines := p9.ReadInput()
+	sum := 0
+	for _, line := range lines {
+		numbers := strings.Split(line, " ")
+		var intSlice []int
+		for _, strNum := range numbers {
+			num, _ := strconv.Atoi(strNum)
+			intSlice = append(intSlice, num)
+		}
+		sum += calculateNextInSequence(intSlice)
+	}
+	fmt.Println("Result: ", sum)
 }
 
 func calculateNextInSequence(seq []int) int {
-  curSeq := &seq
-  var resultSeq []int
-  for p9.HasNonZero(*curSeq) {
-    resultSeq = append(resultSeq, (*curSeq)[0])
-    curSeq = p9.DiffSequence(*curSeq)
-  }
-  p9.Reverse(resultSeq)
-  for i := 1; i < len(resultSeq); i++ {
-    resultSeq[i] = resultSeq[i] - resultSeq[i - 1]
-  }
-  return resultSeq[len(resultSeq) - 1]
+	curSeq := &seq
+	var resultSeq []int
+	for p9.HasNonZero(*curSeq) {
+		resultSeq = append(resultSeq, (*curSeq)[0])
+		curSeq = p9.DiffSequence(*curSeq)
+	}
+	p9.Reverse(resultSeq)
+	for i := 1; i < len(resultSeq); i++ {
+		resultSeq[i] = resultSeq[i] - resultSeq[i-1]
+	}
+	return resultSeq[len(resultSeq)-1]
 }
-
