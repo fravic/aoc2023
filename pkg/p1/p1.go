37814928a9ade75e71fe191afb4dbb44c68aaa2d
Fravic <fravicf@gmail.com>
2023-12-08 23:01:30 -0800

    Fix tabs

--- pkg/p1/p1.go
+++ pkg/p1/p1.go
@@ -8,25 +8,25 @@ import (
 )
 
 var (
-	wordToNumber = map[string]string {
-		"one": "1",
-		"two": "2",
-		"three": "3",
-		"four": "4",
-		"five": "5",
-		"six": "6",
-		"seven": "7",
-		"eight": "8",
-		"nine": "9",
-	}
+  wordToNumber = map[string]string {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+  }
 )
 
 func Run() {
   lines := readInput()
   sum := 0
   for _, line := range lines {
-		convertedLine := insertNumeric(line)
-		fmt.Println(convertedLine)
+    convertedLine := insertNumeric(line)
+    fmt.Println(convertedLine)
     numbers := filterToNumericCharacters(convertedLine)
     result := string(numbers[0]) + string(numbers[len(numbers) - 1])
     num, _ := strconv.Atoi(result)
@@ -52,15 +52,15 @@ func filterToNumericCharacters(line string) string {
 }
 
 func insertNumeric(line string) string {
-	var builder strings.Builder
-	for i := 0; i < len(line); i++ {
-		sliced := line[i:]
-		for word, number := range wordToNumber {
-			if strings.HasPrefix(sliced, word) {
-				builder.WriteString(number)
-			}
-		}
-		builder.WriteRune(rune(sliced[0]))
-	}
-	return builder.String()
+  var builder strings.Builder
+  for i := 0; i < len(line); i++ {
+    sliced := line[i:]
+    for word, number := range wordToNumber {
+      if strings.HasPrefix(sliced, word) {
+        builder.WriteString(number)
+      }
+    }
+    builder.WriteRune(rune(sliced[0]))
+  }
+  return builder.String()
 }
