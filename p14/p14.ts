14d39b32da1a585a2f9a913aad62c676d722624e
Fravic <fravicf@gmail.com>
2023-12-13 22:27:57 -0800

    hax

--- p14/p14.ts
+++ p14/p14.ts
@@ -1,15 +1,18 @@
 import * as fs from "fs";
-import { repeat } from "lodash";
+import { repeat, times } from "lodash";
 
 const file = fs.readFileSync("./p14/input.txt", "utf8");
-const rows = file.split("\n");
+let grid = file.split("\n");
 
-function column(x: number) {
-  let column = "";
-  for (let y = 0; y < rows[0].length; y++) {
-    column += rows[y][x];
+function rotateRight(grid: Array<string>) {
+  let newGrid = [];
+  for (let x = 0; x < grid[0].length; x++) {
+    newGrid[x] = "";
+    for (let y = grid.length - 1; y >= 0; y--) {
+      newGrid[x] += grid[y][x];
+    }
   }
-  return column;
+  return newGrid;
 }
 
 function count(str: string, char: string) {
@@ -20,8 +23,8 @@ function count(str: string, char: string) {
   return count;
 }
 
-function tiltColumnNorth(column: string): string {
-  const splits = column.split("#");
+function tiltRowLeft(row: string): string {
+  const splits = row.split("#");
   let tilted = "";
   for (let i = 0; i < splits.length; i++) {
     const rockCount = count(splits[i], "O");
@@ -33,19 +36,36 @@ function tiltColumnNorth(column: string): string {
   return tilted;
 }
 
-function calculateLoad(column: string): number {
+function calculateLoadOnLeft(row: string): number {
   let sum = 0;
-  for (let y = 0; y < column.length; y++) {
-    if (column[y] === "O") {
-      sum += column.length - y;
+  for (let x = 0; x < row.length; x++) {
+    if (row[x] === "O") {
+      sum += row.length - x;
     }
   }
   return sum;
 }
 
-let sum = 0;
-for (let x = 0; x < rows[0].length; x++) {
-  sum += calculateLoad(tiltColumnNorth(column(x)));
-}
+// Rotate right 2 times to start so north is first
+times(2, () => (grid = rotateRight(grid)));
 
-console.log("Sum: ", sum);
+const CYCLES = 1000;
+for (let i = 0; i < CYCLES; i++) {
+  // Perform a cycle
+  times(4, () => {
+    grid = rotateRight(grid);
+    for (let y = 0; y < grid[0].length; y++) {
+      grid[y] = tiltRowLeft(grid[y]);
+    }
+  });
+
+  // After the end of the cycle, we're oriented with left = EAST, so rotate once more to get north
+  let tempGrid = rotateRight(grid);
+  let sum = 0;
+  for (let y = 0; y < tempGrid[0].length; y++) {
+    sum += calculateLoadOnLeft(tempGrid[y]);
+  }
+
+  // Look for the repeating pattern here
+  console.log(`Sum (${i}): `, sum);
+}
